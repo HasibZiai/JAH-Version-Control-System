feecabfc724b16ffdb6a4d9674e8f2f2c82ad2d6
agumabong <32043546+agumabong@users.noreply.github.com>
2018-12-14 17:23:09 -0800

    Update checkout.h

--- checkout.h
+++ checkout.h
@@ -10,15 +10,15 @@
 
 namespace fs = std::experimental::filesystem;
 
-void check_out(std::string s_path, std::string source_path, std::string label )
+void check_out(std::string s_path, std::string copy_path, std::string label )
 {
 	time_t current_time = time(NULL);
 	char timeStr[26];
 	char cur_time = ctime_s(timeStr, sizeof timeStr, &current_time);
 	std::string label_name;
-	std::string sp = source_path + "\\manifest.txt";
+	std::string sp = copy_path + "\\manifest.txt";
 	
-	if (source_path == s_path)
+	if (copy_path == s_path)
 	{
 		std::cout << "Error, copied path cannot be the same as destination folder." << std::endl;
 	}
@@ -34,18 +34,23 @@ void check_out(std::string s_path, std::string source_path, std::string label )
 		{
 			std::string p = iter->path().string();
 			//if a file is the manifest it will open it and check what the label says
+			//does this by taking the last 12 characters of the filepath and check that its "manifest.txt"
 			if (p.substr(p.size() - 12, 12) == "manifest.txt")
 			{
+				//Check to see if the file is open or not
 				std::ifstream f(p);
 				if (f.is_open() == false)
 				{
 					std::cout << "Failed to open file." << std::endl;
 				}
+				//Loops while there is still content inside of the file
 				while (std::getline(f,line))
 				{
+					//Grabs each line of the text file one by one
 					std::getline(f,line);
 					std::cout << line.substr(0,5) << std::endl;
-					//stores the line that says label
+					//Checks to see if the line is label
+					//if it is label, take the rest of the line as the label
 					if (line.substr(0, 6) == "Label:")
 					{
 						c_label = line.substr(7, line.size() - 1);
@@ -55,7 +60,7 @@ void check_out(std::string s_path, std::string source_path, std::string label )
 				//if the labels match copy the file path to the folder
 				if (c_label == label)
 				{
-					fs::copy(iter->path().parent_path(), source_path, fs::copy_options::recursive);
+					fs::copy(iter->path().parent_path(), copy_path, fs::copy_options::recursive);
 				}
 				
 			}
